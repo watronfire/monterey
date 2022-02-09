b154db4d3ba9875c1d8b312a6de710debd87b864
watronfire <snowboardman007@gmail.com>
2022-02-09 10:34:24 -0800

    run command changed to cluster

--- run_phylosor.sh
+++ run_phylosor.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #SBATCH --time=120:00:00
 #SBATCH --cpus-per-task=1
-#SBATCH --mem=2gb
+#SBATCH --mem=5gb
 #SBATCH --partition=shared
-#SBATCH --output=/gpfs/home/natem/logs/tialoc.txt
+#SBATCH --output=/gpfs/home/natem/logs/phylosor.txt
 
-source /opt/miniconda3/bin/activate &&
-snakemake -k -j 50 --use-conda
+module load python/3.8.3 &&
+snakemake -k -j 50 --use-conda --cluster-config config/cluster.json --cluster "sbatch --time={cluster.walltime} --mem={cluster.mem} -c {cluster.n} --partition={cluster.queue} --output={cluster.logfile}"
