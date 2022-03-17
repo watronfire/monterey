5e1a9ea4c5b7e4a37b5650ea720f7eddb556e328
watronfire <snowboardman007@gmail.com>
2022-03-17 15:57:02 -0700

    fixed combine_results script

--- workflow/scripts/combine_results.py
+++ workflow/scripts/combine_results.py
@@ -7,19 +7,24 @@ def combine_results( results, output ):
     for result in results:
         result_df = pd.read_csv( result, parse_dates=["date"], index_col=0 )
 
-        name = os.path.splitext( os.path.split( result )[1] )[1]
+        name = os.path.splitext( os.path.split( result )[1] )[0]
         name_split = name.split( "." )
+        assert len( name_split ) == 3, f"{name} is oddly formatted."
         result_df["kind"] = name_split[1]
         result_df["num"] = name_split[2]
+        
+        line_count += result_df.shape[0]
+        
         output_df.append( result_df )
 
     output_df = pd.concat( output_df )
 
     assert output_df.shape == output_df.drop_duplicates().shape, "columns are duplicated. Need to make more unique"
+    assert output_df.shape[0] == line_count, f"Entries dropped from concattenated file, {line_count} counted vs. {output_df.shape[0]} present"
 
     output_df.to_csv( output, index=False )
 
 if __name__ == "__main__":
-    results = snakemake.input.results_nulls
-    results.extend( snakemake.input.results_actual )
-    combine_results( results=results, output=snakemake.output.results )
+    results_list = snakemake.input.results_nulls
+    results_list.extend( snakemake.input.results_actual )
+    combine_results( results=results_list, output=snakemake.output.results )
