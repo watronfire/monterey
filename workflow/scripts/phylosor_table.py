5c9451f99b686640e9b08c127206a1f73662199c
watronfire <snowboardman007@gmail.com>
2022-03-21 13:25:30 -0700

    Rewrote to make more modular

--- workflow/scripts/phylosor_table.py
+++ workflow/scripts/phylosor_table.py
@@ -36,50 +36,83 @@ def phylosor( tree, comA, comB ):
     #print( f"blBoth: {blBoth}" )
     #print( f"phylosor: {blBoth / (0.5* (blA + blB) ):.2f}" )
 
-def phylosor_table( tree_loc, md_loc, siteA, siteB, window, output_loc ):
-    print( "Loading tree...", end="")
+
+def load_tree( tree_loc, verbose=True ):
+    if verbose:
+        print( "Loading tree...", end="" )
     starting_time = time.time()
     tree = Tree.get( path=tree_loc, schema="newick", preserve_underscores=True )
-    tip_labels = [i.label for i in tree.taxon_namespace]
-    print( f"Done in {time.time() - starting_time:.1f} seconds")
+    if verbose:
+        print( f"Done in {time.time() - starting_time:.1f} seconds" )
+    return tree
 
-    print( "Loading metadata...", end="" )
-    md = pd.read_csv( md_loc, usecols=["accession_id", "date_collected", "site"], parse_dates=["date_collected"] )
-    md = md.loc[md["accession_id"].isin( tip_labels )]
+
+def load_metadata( md_loc, tip_labels, verbose=True ):
+    if verbose:
+        print( "Loading metadata...", end="" )
     starting_time = time.time()
-    print( f"Done in {time.time() - starting_time:.1f} seconds" )
+    metadata = pd.read_csv( md_loc, usecols=["accession_id", "date_collected", "site"], parse_dates=["date_collected"] )
+    metadata = metadata.loc[metadata["accession_id"].isin( tip_labels )]
+    if verbose:
+        print( f"Done in {time.time() - starting_time:.1f} seconds" )
+    return metadata
 
-    min_date = md.loc[md["site"].isin([siteA, siteB]), "date_collected"].min()
-    max_date = md.loc[md["site"].isin([siteA, siteB]), "date_collected"].max()
-    date_seq = pd.date_range( start=min_date, end=max_date )
-    print( f"Performing {len( date_seq[:-window] )} comparisons with window length {window}" )
+
+def generate_date_seq( metadata, query, verbose=True ):
+    min_date = metadata.loc[query, "date_collected"].min()
+    max_date = metadata.loc[query, "date_collected"].max()
+    return pd.date_range( start=min_date, end=max_date )
+
+
+def phylosor_table( tree, metadata, queryA, nameA, queryB, nameB, window, verbose=True ):
+
+    date_seq = generate_date_seq( metadata, queryA|queryB )
+    if verbose:
+        print( f"Performing {len( date_seq[:-window] )} comparisons with window length {window}" )
+
 
     output_df = list()
     for i in range( len( date_seq[:-window] ) ):
         start_time = time.time()
         comparison = (date_seq[i],date_seq[i+30])
-        communityA = set( md.loc[md["date_collected"].between( *comparison )&(md["site"]==siteA), "accession_id"].to_list() )
-        communityB = set( md.loc[md["date_collected"].between( *comparison )&(md["site"]==siteB), "accession_id"].to_list() )
+        communityA = set( metadata.loc[metadata["date_collected"].between( *comparison )&queryA, "accession_id"].to_list() )
+        communityB = set( metadata.loc[metadata["date_collected"].between( *comparison )&queryB, "accession_id"].to_list() )
 
         if len( communityA ) + len( communityB ) == 0:
-            print( f"{date_seq[i].date} being skipped. No sequences")
+            if verbose:
+                print( f"{date_seq[i].date} being skipped. No sequences")
             continue
 
         entry = phylosor( tree, communityA, communityB )
-        entry.extend( [date_seq[i].strftime( "%Y-%m-%d"), siteA, len( communityA), siteB, len( communityB )] )
+        entry.extend( [date_seq[i].strftime( "%Y-%m-%d"), nameA, len( communityA), nameB, len( communityB )] )
         output_df.append( entry )
 
-        print( f"Completed {i} of {len(date_seq)} comparisons (took {time.time() - start_time:.1f} seconds)." )
+        if verbose:
+            print( f"Completed {i} of {len(date_seq)} comparisons (took {time.time() - start_time:.1f} seconds)." )
 
     output_df = pd.DataFrame( output_df, columns=["blA", "blB", "blBoth", "date", "siteA", "countA", "siteB", "countB" ] )
     output_df["value"] = output_df["blBoth"] / ( 0.5 * (output_df["blA"] + output_df["blB"] ) )
 
-    output_df.to_csv( output_loc )
+    return output_df
 
 if __name__ == "__main__":
-    phylosor_table(tree_loc=snakemake.input.tree,
-                   md_loc=snakemake.input.metadata,
-                   siteA=snakemake.params.pair_list[0],
-                   siteB=snakemake.params.pair_list[1],
-                   window=int( snakemake.params.window_size ),
-                   output_loc=snakemake.output.results )
+    t = load_tree( tree_loc=snakemake.input.tree )
+    tl = [i.label for i in t.taxon_namespace]
+
+    md = load_metadata( md_loc=snakemake.input.metadata, tip_labels=tl )
+
+    # TODO: this will be the place to generate between or within-location queries
+    name_A = snakemake.params.pair_list[0]
+    query_A = md["site"]== name_A
+    name_B = snakemake.params.pair_list[1]
+    query_B = md["site"]== name_B
+
+    output = phylosor_table( tree=t,
+                             metadata=md,
+                             queryA=query_A,
+                             nameA=name_A,
+                             queryB=query_B,
+                             nameB=name_B,
+                             window=int( snakemake.params.window_size ) )
+
+    output.to_csv( snakemake.output.results )
