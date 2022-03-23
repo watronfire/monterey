732569c5c796cf28f9e39ec5df4e7d385d56119a
nate <snowboardman007@gmail.com>
2022-03-23 14:22:34 -0700

    Cleaned up JS function

--- workflow/scripts/jensen_shannon_distance.py
+++ workflow/scripts/jensen_shannon_distance.py
@@ -1,10 +1,15 @@
+import pandas as pd
+import time
+from epiweeks import Week
+from scipy.spatial.distance import jensenshannon
+import numpy as np
 
 def load_metadata( md_loc, pair, verbose=True ):
     if verbose:
         print( "Loading metadata...", end="" )
     starting_time = time.time()
     metadata = pd.read_csv( md_loc, usecols=["accession_id", "date_collected", "pangolin_lineage", "site"], parse_dates=["date_collected"] )
-    metadata = metadata.loc[metadata["site"].isin([pair])]
+    metadata = metadata.loc[metadata["site"].isin(pair)]
     metadata["week"] = metadata["date_collected"].apply( lambda x: Week.fromdate( x ).startdate() )
     metadata["month"] = metadata["date_collected"].to_numpy().astype('datetime64[M]')
     if verbose:
@@ -20,10 +25,8 @@ def genome_bootstrap( df, func, replicates=100 ):
     return {k:j for k,j in zip( ["js_5", "js_mean", "js_95"], res )}
 
 def apply_jensenshannon( df ):
-    df = df.groupby( ["site", "pangolin_lineage"] )["accession_id"].agg( "count" )
-    evaluate = df.reset_index().pivot( columns="site", index="pangolin_lineage", values="accession_id" ).fillna(0)
+    evaluate = df.pivot_table( index="pangolin_lineage", columns="site", values="accession_id", aggfunc="count", fill_value=0 )
     evaluate = evaluate.apply( lambda x: x / x.sum() )
-
     try:
         return jensenshannon( evaluate.iloc[:,0], evaluate.iloc[:,1], base=2 )
     except IndexError:
