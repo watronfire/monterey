4632ada98314d94bd838b3391819cd7ac1c5e105
watronfire <snowboardman007@gmail.com>
2022-06-20 14:47:58 -0400

    Pipeline is split now.

--- workflow/scripts/generate_pairs.py
+++ workflow/scripts/generate_pairs.py
@@ -65,9 +65,9 @@ def generate_pairs( md_loc, min_sequences, min_completeness, output_loc, graph_l
 
     prepare_graph( summary, min_sequences, min_completeness, graph_loc )
 
-    selected = summary.loc[(summary["completeness"]>0.75)&(summary["sequences"]>1000)&(summary.index != "San Diego County")].index
+    selected = summary.loc[(summary["completeness"]>0.75)&(summary["sequences"]>1000)&(summary.index != "San Diego")].index
     with open( output_loc, "w" ) as output:
-        [output.write( f"San Diego County,{i}\n" ) for i in selected]
+        [output.write( f"San Diego,{i}\n" ) for i in selected]
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser( description="Generate location pairs from metadata with reasonable sampling" )
