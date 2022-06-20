5e74ff084709b71ba37275f54861809c16b72541
watronfire <snowboardman007@gmail.com>
2022-06-20 10:15:34 -0400

    More robust location search

--- workflow/rules/phylosor_newnull.smk
+++ workflow/rules/phylosor_newnull.smk
@@ -1,10 +1,88 @@
+rule generate_metadata:
+    message: "Remove entries in metadata that aren't found in tree."
+    input:
+        metadata = config["input_locations"]["metadata"],
+        early_chn = config["input_locations"]["chn_metadata"]
+    output:
+        combined_metadata = "resources/combined_md.csv"
+    run:
+        import pandas as pd
+        ec = pd.read_csv( input.early_chn, parse_dates=["date_collected"] )
+
+        md = pd.read_csv( input.metadata, parse_dates=["date_collected"] )
+        md = md.drop( columns="Unnamed: 0" )
+        md = pd.concat( [md, ec] )
+        md["strain"] = md["strain"].str.slice( start=8 )
+        md.to_csv( output.combined_metadata, index=False )
+
+rule metadata_prune:
+    message: "Remove tips of tree that aren't found in metadata"
+    input:
+        tree = config["input_locations"]["tree"],
+        metadata = rules.generate_metadata.output.combined_metadata
+    output:
+        missing = "intermediates/metadata_prune/missing.txt",
+        tree = "intermediates/metadata_prune/cog_md.tree",
+    run:
+        import numpy as np
+        md = pd.read_csv( input.metadata, parse_dates=["date_collected"] )
+
+        tree_tips = [i for i in shell( "gotree labels < {input.tree}", iterable=True )]
+
+        # Identify tips that are in tree but aren't in metadata
+        missing = np.setdiff1d( tree_tips, md["strain"].to_list() )
+        print( f"{len(missing)} of {len(md['strain'])} tips missing" )
+
+        with open( output.missing, "w" ) as missing_file:
+            missing_file.write( "\n".join( missing ) )
+
+        # Prune tips that are missing
+        shell( "gotree prune --tipfile {output.missing} < {input.tree} > {output.tree}" )
+
+
+rule rename_tree_to_accession:
+    message: "Rename tips of tree to accession IDs from strain names"
+    input:
+        tree = rules.metadata_prune.output.tree,
+        metadata = rules.generate_metadata.output.combined_metadata
+    output:
+        renames = "intermediates/rename_tree/renames.tsv",
+        tree = "intermediates/rename_tree/cog_accession.tree"
+    shell:
+        """
+        cut -f1,2 -d, combined_md.csv | sed "s/,/\t/g" > {output.renames} &&
+        gotree rename --map {output.renames} < {input.tree} > {output.tree}
+        """
+
+rule generate_pairs:
+    message: "Do a robust search through the metadata for all locations which have greater than {params.sequences} and greater than {params.completeness} epiweeks covered."
+    conda: "../envs/general.yaml"
+    log: "logs/generate_pairs.txt"
+    input:
+        metadata = rules.generate_metadata.output.combined_metadata
+    params:
+        sequences = config["pairs"]["min_sequences"],
+        completeness = config["pairs"]["min_completeness"]
+    output:
+        pairs = "intermediates/pairs/pairs.txt",
+        pair_graph = "results/reports/pair_graph.pdf"
+    shell:
+        """
+        python workflow/scripts/generate_pairs.py \
+            --metadata {input.metadata} \
+            --min-sequences {params.sequences} \
+            --min-completeness {params.completeness} \
+            --output {output.pairs} \
+            --graph {output.pair_graph}
+        """
+
 rule prune_tree_to_pair_newnull:
     message: "Prune tree to only sequences from pair: {wildcards.pair}"
     conda: "../envs/general.yaml"
     log: "logs/{pair}.pruning.txt"
     input:
-        tree = config["input_locations"]["tree"],
-        metadata = config["input_locations"]["metadata"]
+        tree = rules.metadata_prune.output.tree,
+        metadata = rules.generate_metadata.output.combined_metadata
     params:
         pair_list = lambda wildcards: PAIRS[wildcards.pair],
         id_col = config["columns"]["id_col"],
@@ -32,7 +110,7 @@ rule compute_phylosor_newnull:
     log: "logs/{pair}.{status}.{num}.phylosor_newnull.txt"
     input:
         tree = rules.prune_tree_to_pair_newnull.output.pruned_tree,
-        metadata = config["input_locations"]["metadata"]
+        metadata = rules.generate_metadata.output.combined_metadata
     params:
         pair_list = lambda wildcards: PAIRS[wildcards.pair],
         window_size = config["compute_phylosor"]["window_size"],
---
+++ workflow/scripts/generate_pairs.py
@@ -0,0 +1,102 @@
+import argparse
+
+import pandas as pd
+from epiweeks import Week
+from matplotlib import pyplot as plt
+from matplotlib.ticker import PercentFormatter
+
+
+def load_metadata( md_loc ):
+    usecols = ["accession_id", "date_collected", "country", "division", "location",
+               "pangolin_lineage", "site"]
+    na_countries = ['Antigua and Barbuda', 'Bahamas', 'Barbados', 'Belize',
+                    'Bermuda', 'Canada', 'Costa Rica', 'Cuba',
+                    'Dominica', 'Dominican Republic', 'El Salvador', 'Guadeloupe', 'Grenada',
+                    'USA', 'Guatemala', 'Haiti', 'Honduras',
+                    'Jamaica', 'Mexico', 'Panama', 'Saint Barthélemy', 'Saint Kitts and Nevis',
+                    'Saint Lucia', 'Saint Martin', 'Saint Vincent and the Grenadines', 'Sint Maarten' ]
+    md = pd.read_csv( md_loc, usecols=usecols, parse_dates=["date_collected"])
+    md["week"] = md["date_collected"].apply( lambda x: Week.fromdate( x ).startdate() )
+    md = md.loc[md["country"].isin(na_countries)]
+    md["site"] = md["country"]
+    md.loc[md["country"].isin( ["Canada", "Mexico"] ), "site"] = md["division"]
+    md.loc[md["country"] == "USA", "site"] = md["location"]
+    md.loc[md["division"].isin( ["Guam", "Puerto Rico", "Virgin Islands", "Northern Mariana Islands"] ), "site"] = md["division"]
+    md = md.loc[md["site"] != "None"]
+    return md
+
+
+def calculate_summary( md ):
+    # sequences per site
+    seqs = md["site"].value_counts()
+
+    # fraction of sequences per week per site.
+    completeness = md.pivot_table( columns="site", index="week", values="region", aggfunc="count" )
+    completeness = completeness.loc[completeness.index > pd.to_datetime( "2020-02-23" )]
+    completeness = completeness.count() / completeness.shape[0]
+
+    return_df = pd.concat( [seqs, completeness], axis=1, ignore_index=False )
+    return_df.columns = ["sequences", "completeness"]
+
+    return_df["country"] = 'USA'
+    return_df.loc[md.loc[md["country"] == "Canada","division"].unique(), "country"] = "Canada"
+    return_df.loc[md.loc[md["country"] == "Mexico","division"].unique(), "country"] = "Mexico"
+    return return_df
+
+
+def prepare_graph( summary, min_sequences, min_completeness, graph_loc ):
+    fig, ax = plt.subplots( dpi=200, figsize=(6, 4) )
+    ax.scatter( "sequences", "completeness", data=summary.loc[summary["country"] == "USA"],
+                label="United States" )
+    ax.scatter( "sequences", "completeness", data=summary.loc[summary["country"] == "Mexico"],
+                label="Mexico" )
+    ax.scatter( "sequences", "completeness", data=summary.loc[summary["country"] == "Canada"],
+                label="Canada" )
+    ax.scatter( "sequences", "completeness", data=summary.loc["San Diego County"], label="San Diego" )
+    ax.set_ylabel( "Proportion epiweeks sequenced" )
+    ax.set_xlabel( "Total sequences" )
+    ax.axvline( 1000, linestyle="dashed", linewidth=1, color="red" )
+    ax.axhline( 0.75, linestyle="dashed", linewidth=1, color="red" )
+    ax.text( 900, 0.95, ">1000 genomes", ha="right", color="red" )
+    ax.text( 1, 0.77, ">75% epiweeks", color="red" )
+    ax.set_xscale( "log" )
+    ax.yaxis.set_major_formatter( PercentFormatter( 1, 0 ) )
+    ax.set_ylim( 0, 1 )
+    ax.legend( frameon=False )
+    plt.tight_layout()
+    plt.savefig( graph_loc )
+
+
+def generate_pairs( md_loc, min_sequences, min_completeness, output_loc, graph_loc ):
+    # Load metadata and add site information.
+    md = load_metadata( md_loc )
+
+    # Calculate sequences and completeness per site
+    summary = calculate_summary( md )
+
+    if graph_loc:
+        prepare_graph( summary, min_sequences, min_completeness, graph_loc )
+
+    selected = summary.loc[(summary["completeness"]>0.75)&(summary["sequences"]>1000)&(summary.index != "San Diego County")].index
+    with open( output_loc, "w" ) as output:
+        [output.write( f"San Diego County,{i}" ) for i in selected]
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser( description="Generate location pairs from metadata with reasonable sampling" )
+
+    # Initialize arguments
+    parser.add_argument( "--metadata", help="location of metadata", required=True )
+    parser.add_argument( "--min-sequences", help="Keep locations with at least this many sequences", required=True )
+    parser.add_argument( "--min-completeness", help="Keep locations with sequences collected from at least this many epiweeks", required=True )
+    parser.add_argument( "--output", help="location to save pairs", required=True )
+    parser.add_argument( "--graph", action="store_true", help="location to save diagnostic plot" )
+
+    args = parser.parse_args()
+
+    generate_pairs(
+        md_loc=args.metadata,
+        min_sequences=args.min_sequences,
+        min_completeness=args.min_completeness,
+        output_loc=args.output,
+        graph_loc=args.graph
+    )
