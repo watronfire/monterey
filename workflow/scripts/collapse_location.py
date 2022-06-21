fcbdc18ee5d4b5355a124565ab2798fc52c6ab78
watronfire <snowboardman007@gmail.com>
2022-06-21 11:06:36 -0400

    Added state name to site to avoid collisions

--- workflow/scripts/collapse_location.py
+++ workflow/scripts/collapse_location.py
@@ -2,6 +2,68 @@ import argparse
 
 import pandas as pd
 
+us_state_to_abbrev = {
+    "Alabama": "AL",
+    "Alaska": "AK",
+    "Arizona": "AZ",
+    "Arkansas": "AR",
+    "California": "CA",
+    "Colorado": "CO",
+    "Connecticut": "CT",
+    "Delaware": "DE",
+    "Florida": "FL",
+    "Georgia": "GA",
+    "Hawaii": "HI",
+    "Idaho": "ID",
+    "Illinois": "IL",
+    "Indiana": "IN",
+    "Iowa": "IA",
+    "Kansas": "KS",
+    "Kentucky": "KY",
+    "Louisiana": "LA",
+    "Maine": "ME",
+    "Maryland": "MD",
+    "Massachusetts": "MA",
+    "Michigan": "MI",
+    "Minnesota": "MN",
+    "Mississippi": "MS",
+    "Missouri": "MO",
+    "Montana": "MT",
+    "Nebraska": "NE",
+    "Nevada": "NV",
+    "New Hampshire": "NH",
+    "New Jersey": "NJ",
+    "New Mexico": "NM",
+    "New York": "NY",
+    "North Carolina": "NC",
+    "North Dakota": "ND",
+    "Ohio": "OH",
+    "Oklahoma": "OK",
+    "Oregon": "OR",
+    "Pennsylvania": "PA",
+    "Rhode Island": "RI",
+    "South Carolina": "SC",
+    "South Dakota": "SD",
+    "Tennessee": "TN",
+    "Texas": "TX",
+    "Utah": "UT",
+    "Vermont": "VT",
+    "Virginia": "VA",
+    "Washington": "WA",
+    "West Virginia": "WV",
+    "Wisconsin": "WI",
+    "Wyoming": "WY",
+    "District of Columbia": "DC",
+    "Washington DC": "DC",
+    "American Samoa": "AS",
+    "Guam": "GU",
+    "Northern Mariana Islands": "MP",
+    "Puerto Rico": "PR",
+    "United States Minor Outlying Islands": "UM",
+    "U.S. Virgin Islands": "VI",
+    "Virgin Islands, U.S.": "VI",
+    "Virgin Islands": "VI",
+}
 
 def collapse_metadata( md_loc, output ):
     na_countries = ['Antigua and Barbuda', 'Bahamas', 'Barbados', 'Belize',
@@ -16,6 +78,8 @@ def collapse_metadata( md_loc, output ):
     md.loc[md["country"].isin( ["Canada", "Mexico"] ), "site"] = md["division"]
     md.loc[md["country"] == "United States", "site"] = md["location"]
     md.loc[md["division"].isin( ["Guam", "Puerto Rico", "Virgin Islands", "Northern Mariana Islands"] ), "site"] = md["division"]
+
+    md.loc[(md["country"]=="United States"),"site"] = md.loc[(md["country"]=="United States"),"site"] + "_" + md.loc[(md["country"]=="United States"),"division"].map( us_state_to_abbrev )
 
     early = md.sort_values( "date_collected" ).head(50)["accession_id"].to_list()
     md.loc[md["accession_id"].isin( early ),"site"] = "root"
